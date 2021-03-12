fd73ad4b56a571845975718acad3ad3d9311cc3a
luiscarlsoto <luiscarlsoto@gmail.com>
2021-03-12 15:35:44 -0400

    agregada las query desde el servidor

--- server/server.js
+++ server/server.js
@@ -15,9 +15,28 @@ const app = express()
 var cors = require('cors')
 app.use(cors())
 
-app.get('/product', (req, res) =>{})
+app.get('/product', (req, res) =>{
+    try {
+        connection.query('SELECT * FROM product', (err, results, field) => {
+            res.send(JSON.stringify({"status": 200, "error": null, "response": results}
+            ));
+        
+    });
+    } catch (error) {
+        res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+    }
+})
 
-app.get('/category', (req, res) => {});
+app.get('/category', (req, res) =>{
+        try {
+            connection.query('SELECT * FROM category', (err, results, field) => {
+                res.send(JSON.stringify({"status": 200, "error": null, "response": results
+            }));
+        });
+        } catch (error) {
+            next(error);
+        }    
+    });
 
 app.listen(port, () => {
     console.log(`Ejecutando servidor en el puerto ${port}...`);
